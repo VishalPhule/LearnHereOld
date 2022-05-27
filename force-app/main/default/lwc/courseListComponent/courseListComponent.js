01ef3d2e244055ca500dabe700df0bd0ebe43a6f
Vishal Phule <vishalphule2000@gmail.com>
2022-05-27 23:07:50 +0530

    Seconf commit

---
+++ force-app/main/default/lwc/courseListComponent/courseListComponent.js
@@ -0,0 +1,61 @@
+
+import { LightningElement, wire } from 'lwc';
+import fetchUpComingCourses from '@salesforce/apex/CourseListLWCService.fetchUpComingCourses';
+import fetchPastCourses from '@salesforce/apex/CourseListLWCService.fetchPastCourses';
+
+import { NavigationMixin } from 'lightning/navigation';
+
+//import CourseTitle from '@salesforce/resourceUrl/CourseTitle';
+
+export default class CourseListComponent extends NavigationMixin(LightningElement) {
+
+    upcomingCourse;
+    pastCourses;
+    __errors;
+    isSpinner = false;
+
+    // images = {
+    //     course : CourseTitle
+    // }
+
+    @wire(fetchUpComingCourses)
+    wiredUpComingCoursesData({ error, data }) {
+        if (data) {
+            this.upcomingCourse = data;
+        } else if (error) {
+            console.error('Course listcmpnt Error:', error);
+            this.upcomingCourse = undefined;
+            this.__errors = error;
+        }
+    }
+
+    @wire(fetchPastCourses)
+    wiredPastCoursesData({ error, data }) {
+        if (data) {
+            this.pastCourses = data;
+        } else if (error) {
+            console.error('Course listcmpnt Past Event Error:', error);
+            this.pastCourses = undefined;
+            this.__errors = error;
+        }
+    }
+
+    handleourseClick = Course => {
+
+        Course.preventDefault();
+        let selectedCourseId= Course.detail.courseId;
+        alert('Helllo...' + selectedCourseId);
+        let navigationTarget = {
+            type: 'comm__namedPage',
+            attributes: {
+                name: "CourseDetails__c"
+            },
+            state: {
+                courseId: selectedCourseId,
+                source: 'CourseListPage'
+            }
+        }
+
+        this[NavigationMixin.Navigate](navigationTarget);
+    }
+}
